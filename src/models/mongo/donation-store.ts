647d55ab49f1f586e02c7c9250c47d3d31ccc66b
dermie83 <dermiemadsen@gmail.com>
2024-04-25 08:19:48 +0100

    initial change over to ts

---
+++ src/models/mongo/donation-store.ts
@@ -0,0 +1,25 @@
+import { DonationMongoose } from "./donation.js";
+import { Donation } from "../../types/donation-types.js";
+
+export const donationStore = {
+  async find() {
+    const donations = await DonationMongoose.find().populate("donor").populate("candidate").lean();
+    return donations;
+  },
+
+  async findBy(id: string) {
+    const donations = await DonationMongoose.find({ candidate: id });
+    return donations;
+  },
+
+  async add(donation: Donation) {
+    let newDonation = new DonationMongoose({ ...donation });
+    await newDonation.save();
+    newDonation = await DonationMongoose.findOne({ _id: newDonation._id }).populate("candidate").lean();
+    return newDonation;
+  },
+
+  async delete() {
+    await DonationMongoose.deleteMany({});
+  },
+};
